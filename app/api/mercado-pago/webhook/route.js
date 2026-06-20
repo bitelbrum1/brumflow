d0039323b3743ffff33b0838f213b4b952a89c21
Gustavo Bitelbrum <gbitelbrum@gmail.com>
2026-06-20 11:19:11 -0300

    debug do debug

--- app/api/mercado-pago/webhook/route.js
+++ app/api/mercado-pago/webhook/route.js
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 console.log("BODY:", body);
 
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
-);
-console.log(
-  "SUPABASE URL:",
-  process.env.NEXT_PUBLIC_SUPABASE_URL
 );
 
 async function liberarPlano(paymentId) {
